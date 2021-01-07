e0ea8d142bcd7807c8e0b744bcc4f8701887401e
Mario Lasić <mario.lasic95@gmail.com>
2021-01-07 15:53:12 +0100

    Added rollDice Function

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.btn_roll)
         rollButton.text = "Roll a dice!"
-        rollButton.setOnClickListener { Toast.makeText(this,"Button clicked!",Toast.LENGTH_SHORT).show() }
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val diceText: TextView = findViewById(R.id.txt_diceRoll)
+        val randomInt = Random().nextInt(6) + 1
+        diceText.text = randomInt.toString()
     }
 }
